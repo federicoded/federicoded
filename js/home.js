6336bbc9fc4ca3fa1de8319900ea8ab4a0333611
Federico Guigou <fefogs@gmail.com>
2025-07-23 00:44:04 -0300

    avance sobre inicio y seccion work

--- js/home.js
+++ js/home.js
@@ -1,11 +1,27 @@
-function verificar(){
-    console.log(navigator.geolocation);
-    navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position.coords.latitude, position.coords.longitude);
-        console.log(position);
-    });
-}
+
+window.scrollTo(0, 0);
+
+window.addEventListener('scroll', function () {
+    const scrollPos = window.scrollY;
+
+    const cartel = document.getElementById('cartel');
+
+    // Cuánto se va a mover como máximo (en px)
+    const maxDesplazamiento = 1000;
+
+    // Factor de movimiento (cuánto se desplaza por pixel de scroll)
+
+    const desplazamiento = Math.min(scrollPos, maxDesplazamiento);
+
+    const alturaMin = 85; // en vh
+    const alturaMax = 110; // en vh
+    const alturaActual = alturaMin + ((alturaMax - alturaMin) * (desplazamiento / maxDesplazamiento));
+    cartel.style.height = `${alturaActual}vh`;
+    console.log(window.scrollY);
+    console.log("desplazamiento:"+desplazamiento);
+
+    // Se mueve hacia la izquierda (negativo)
+    cartel.style.transform = `translateX(${desplazamiento*2}px)`;
 
 
-console.log(navigator.language); // Ej: "es-AR", "en-US"
-verificar();
+});

d38e887c0d81e9ec45873e4536269ed51bef6def
Federico Guigou <fefogs@gmail.com>
2025-07-23 20:45:23 -0300

    primera versión oficial terminada

--- js/home.js
+++ js/home.js
@@ -6,22 +6,16 @@ window.addEventListener('scroll', function () {
 
     const cartel = document.getElementById('cartel');
 
-    // Cuánto se va a mover como máximo (en px)
     const maxDesplazamiento = 1000;
 
-    // Factor de movimiento (cuánto se desplaza por pixel de scroll)
 
     const desplazamiento = Math.min(scrollPos, maxDesplazamiento);
 
-    const alturaMin = 85; // en vh
-    const alturaMax = 110; // en vh
+    const alturaMin = 85; 
+    const alturaMax = 110; 
     const alturaActual = alturaMin + ((alturaMax - alturaMin) * (desplazamiento / maxDesplazamiento));
     cartel.style.height = `${alturaActual}vh`;
-    console.log(window.scrollY);
-    console.log("desplazamiento:"+desplazamiento);
 
-    // Se mueve hacia la izquierda (negativo)
     cartel.style.transform = `translateX(${desplazamiento*2}px)`;
-
 
 });

83a7a0d38366071c0277993e90ea57ca188e17cb
Federico Guigou <fefogs@gmail.com>
2025-07-28 20:48:32 -0300

    correcciones en diseño responsive

--- js/home.js
+++ js/home.js
@@ -4,73 +4,72 @@ console.log(window.devicePixelRatio);
 
 
 window.addEventListener('scroll', moverCartelConScroll);
-
-function moverCartelConScroll() {
-    const scrollPos = window.scrollY;
-
-    const cartel = document.getElementById('cartel');
-
-    const maxDesplazamiento = 1000;
-
-
-    const desplazamiento = Math.min(scrollPos, maxDesplazamiento);
-
-    const alturaMin = 85; 
-    const alturaMax = 110; 
-    const alturaActual = alturaMin + ((alturaMax - alturaMin) * (desplazamiento / maxDesplazamiento));
-    cartel.style.height = `${alturaActual}vh`;
-
-    cartel.style.transform = `translateX(${desplazamiento*2}px)`;
-
-}
-
-const ratio_de_smartphone = window.matchMedia('(max-aspect-ratio: 38/29)');
-
-function actualizarSVG() {
-    const relacion = window.innerWidth / window.innerHeight;
-    const objeto_pajaritos = document.getElementById("pajaritos");
-
-    if (relacion > 38/29) {
-        objeto_pajaritos.style.width = "5.5vw";
-        objeto_pajaritos.data = "assets/pajaritos.svg";
-
-    } else {
-        objeto_pajaritos.style.width = "3vw";
-        objeto_pajaritos.data = "assets/plane.svg";
-    }
-}
-
-
 window.addEventListener("load", actualizarSVG);
 window.addEventListener("resize", actualizarSVG);
 
 
-/*
-const 
-
-CARTEL = document.getElementById("cartel"),
-WELCOME = document.getElementById("welcome"),
-ABOUT_ME = document.getElementById("about_me"),
-PRESENTATION = document.getElementById("presentation"),
-CALLE = document.getElementById("calle"),
-PAJAROS = document.getElementById("container_pajaritos");
-
-const LINKS = document.querySelectorAll(".enlace");
-LINKS.forEach(enlace => {
-    enlace.addEventListener("click", abandonar);
-});
-
-async function abandonar(event) {
-  event.preventDefault();
-    window.removeEventListener('scroll', moverCartelConScroll);
-  const href = event.currentTarget.href; 
-  /// Aca va el overlay que tapa todo y se tiene que destapar cuando inicia cada pagina.
-
-  setTimeout(() => {
-    window.location.href = href;
-  }, 2500);
+function moverCartelConScroll() {
+  const desplazamiento = window.scrollY;
+  const cartel = document.getElementById('cartel');
+  const 
+    maxDesplazamiento = 500, 
+    alturaMin = 85, 
+    alturaMax = 115
+  ; 
+  const altura_nueva = alturaMin + ((alturaMax - alturaMin) * (desplazamiento / maxDesplazamiento));
+  
+  console.log(desplazamiento);
+  cartel.style.height = `${altura_nueva}vh`;
+  cartel.style.transform = `translateX(${desplazamiento*2}px)`;
+  
+  const altura_body = document.body.scrollHeight;
+  console.log(altura_body);
+  if(desplazamiento > (altura_body*0.3)){
+    cambiar_letras_header(false);
+  }else{
+    cambiar_letras_header(true);
+  }
 }
-*/
 
 
- 
+function cambiar_letras_header(opcion){
+  const links = document.querySelectorAll('.header_link');
+  let 
+    color = "",
+    clase_anterior="",
+    clase_nueva =""
+  ;
+  if(opcion){
+    color = "#F2F2F2";
+    clase_anterior = "hover_fondo_claro";
+    clase_nueva = "hover_fondo_oscuro";
+  } else{
+    color = "black";
+    clase_anterior = "hover_fondo_oscuro";
+    clase_nueva = "hover_fondo_claro";
+  }
+  links.forEach( enlace=> {
+    enlace.style.color = color;
+    enlace.classList.remove(clase_anterior);
+    enlace.classList.add(clase_nueva);
+  });
+}
+
+
+const ratio_de_smartphone = window.matchMedia('(max-aspect-ratio: 38/29)');
+
+function actualizarSVG() {
+  const relacion = window.innerWidth / window.innerHeight;
+  const objeto_pajaritos = document.getElementById("pajaritos");
+
+  if (relacion > 38/29) {
+  objeto_pajaritos.style.width = "5.5vw";
+  objeto_pajaritos.data = "assets/pajaritos.svg";
+
+  } else {
+  objeto_pajaritos.style.width = "3vw";
+  objeto_pajaritos.data = "assets/plane.svg";
+  }
+}
+
+

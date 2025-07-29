e61b4d31d662451a0510c90246470d2460805840
Federico Guigou <fefogs@gmail.com>
2025-07-29 03:06:26 -0300

    english translation

---
+++ js/general.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', traducir);
+
+function traducir() {
+  const path = window.location.pathname;
+
+  let 
+    archivo = path.substring(path.lastIndexOf('/') + 1),
+    ruta = "translation/"
+  ;
+
+  archivo = archivo.replace('.html', '');
+
+  if (path.includes("proyects")) {
+    ruta="../translation/";
+  }
+
+  const user_lang = navigator.language.slice(0, 2);
+  if (user_lang == 'es') return;
+
+
+  fetch(ruta+archivo+'_en.json').then(response => response.json())
+    .then(translations => {
+      document.querySelectorAll('[data-i18n]').forEach(elemento => {
+
+        const clave = elemento.getAttribute('data-i18n');
+        elemento.innerHTML = translations[clave];
+      });
+  });
+}
